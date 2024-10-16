93b34d9258e3eb52e75fdc5423240fe530cef104
yasin abbasspour <yasin.absr83@gmail.com>
2024-10-16 15:45:20 +0330

    make beter

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,8 +4,8 @@ function Layout({children}) {
   return (
     <>
     <header className={styles.header}>
-        <h1>Crypto App</h1>
-        <p><a href="https://botostart.ir">Botostart</a> | React.js Full Cours</p>
+        <h1>Yasin Crypto</h1>
+        
     </header>
     {children}
     <footer className={styles.footer}>
